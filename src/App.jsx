fc591c0b9e6e851f5b991a868d58477caef76557
jsweb4dev <jsweb4dev@gmail.com>
2025-12-24 09:40:09 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,48 @@ import { useCallback, useEffect, useRef, useState } from 'react'
     const [isClicked, setIsClikced] = useState(false);
     const boxRef = useRef(null);
 
+    
     //inner function to component to handle document click
     // sets x and y to 0 axis objects
-    const handleDocumentClick = (event) => {
-        setPosition({x: event.clientX, y: event.clientY}) 
+    const handleSingleClick = (event) => {
+        setPosition({x: event.clientX, y: event.clientY})
+        boxRef.current = position
         setIsClikced(true)
       }
 
-    const handleDocumentMouseMove = () => {
+    const handleDoubleClick = () => {
 
     // Hide the box as soon as the mouse moves
+      setPosition({x: -50, y: -10})
       setIsClikced(false);
   }
 
-  
+    const handleBoxOne = () => {
+      //alert("You clicked on box one")
+      console.log(`One wuz clicked + x: ${position.x} y: ${position.y}`)
+    }
+
+    const handleBoxTwo = () => {
+      //alert("You clicked on box one")
+      console.log("Two wuz clicked")
+    }
+
+    const handleBoxThree = () => {
+      //alert("You clicked on box one")
+      console.log("Three wuz clicked")
+    }
+
+
 //useEffect handles the side effects of unwanted rendors
     useEffect(() => {
-      document.addEventListener('click', handleDocumentClick);//if clicked show bo
+      document.addEventListener('click', handleSingleClick);//if clicked show bo
       if (isClicked) {
-        window.addEventListener('dblclick', handleDocumentMouseMove) //mousemove remove box
+        window.addEventListener('dblclick', handleDoubleClick) //mousemove remove box
       }
 
       return () => { //remove event listerners
-        document.removeEventListener('mousemove', handleDocumentClick);
-        window.removeEventListener('mousemove', handleDocumentMouseMove);
+        document.removeEventListener('mousemove', handleSingleClick);
+        window.removeEventListener('mousemove', handleDoubleClick);
       };
     }, [isClicked])
 
@@ -48,20 +66,26 @@ import { useCallback, useEffect, useRef, useState } from 'react'
               style={{
                 position:'fixed',
                 display: 'block',
-                left: position.x,
-                top: position.y,
-                width: '150px',
-                height: '100px',
-                backgroundColor: 'lightblue',
+                left: boxRef.current.x,
+                top: boxRef.current.y,
+                width: '75px',
+                height: '180px',
+                backgroundColor: 'black',
                 // Center the box on the cursor position
                 transform: 'translate(-50%, -50%)',
-                pointerEvents: 'none', // Allows events to pass through the box to the document
+                pointerEvents: 'auto', // Allows events to pass through the box to the document
                 zIndex: 190
               }}
           >
-            <div className="row-item">Row 1</div>
-            <div className="row-item">Row 2</div>
-            <div className="row-item">Row 3</div>
+            <div className="row-item" onClick={handleBoxOne} >
+              <img src='/option1.png' />
+            </div>
+            <div className="row-item" onClick={handleBoxTwo} >
+              <img src='/option2.png' />
+              </div>
+            <div className="row-item" onClick={handleBoxThree} >
+              <img src='/option3.png' />
+              </div>
           </div>
           
         </>
